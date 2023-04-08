a13de84b36eafaefac1950218ac68f7b528d2c4c
Finn Morris <finn@Finns-Laptop.local>
2023-04-08 18:00:46 +0800

    removed empty {}

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import type { AppProps } from 'next/app'
 import "~/styles/globals.css";
 import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+export type NextPageWithLayout<P = {[k: string | number | symbol]: never}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
 }
 
@@ -26,7 +26,6 @@ function MyApp({
         {layout}
       </SignedIn>
       <SignedOut>
-
         <RedirectToSignIn />
       </SignedOut>
     </ClerkProvider>
