5e7f5f0398f03f10a62bcff6398b03402ecc5e35
Finn Morris <finn@Finns-Laptop.local>
2023-09-02 23:45:39 +0800

    added props external reference to time picker

--- src/components/DurationPicker.tsx
+++ src/components/DurationPicker.tsx
@@ -9,23 +9,20 @@ export interface Props {
     setDurationSeconds: (seconds: number) => void;
 }
  function DurationPicker(props: Props) {
-    const [hours, setHours] = useState(0);
-    const [minutes, setMinutes] = useState(0);
-    const [seconds, setSeconds] = useState(0);
 
     const handleHoursChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = parseInt(e.target.value, 10);
-        setHours(isNaN(value) ? 0 : value);
+        props.setDurationHours(isNaN(value) ? 0 : value);
     };
 
     const handleMinutesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = parseInt(e.target.value, 10);
-        setMinutes(isNaN(value) ? 0 : value);
+        props.setDurationMinutes(isNaN(value) ? 0 : value);
     };
 
     const handleSecondsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = parseInt(e.target.value, 10);
-        setSeconds(isNaN(value) ? 30 : value);
+        props.setDurationSeconds(isNaN(value) ? 30 : value);
     };
 
     return (
@@ -34,7 +31,7 @@ export interface Props {
                 h:
                 <input className="dark:bg-primary dark:text-light focus:ring-primary mb-5 mr-2 ml-2 mt-2 h-8 w-1/5 items-center rounded  border border-gray-300 pl-3 text-sm font-normal text-gray-600 placeholder-gray-400 focus:outline-none focus:ring dark:border-gray-700 dark:placeholder-gray-200"
                     type="number"
-                    value={hours}
+                    value={props.durationHours}
                     onChange={handleHoursChange}
                     />
             </label>
@@ -42,7 +39,7 @@ export interface Props {
                 m:
                 <input className="dark:bg-primary dark:text-light focus:ring-primary mb-5 mr-2 ml-2 mt-2 h-8 w-1/5 items-center rounded  border border-gray-300 pl-3 text-sm font-normal text-gray-600 placeholder-gray-400 focus:outline-none focus:ring dark:border-gray-700 dark:placeholder-gray-200"
                     type="number"
-                    value={minutes}
+                    value={props.durationMinutes}
                     onChange={handleMinutesChange}
                      />
             </label>
@@ -50,7 +47,7 @@ export interface Props {
                 s:
                 <input className="dark:bg-primary dark:text-light focus:ring-primary mb-5 mr-2 ml-2 mt-2 h-8 w-1/5 items-center rounded  border border-gray-300 pl-3 text-sm font-normal text-gray-600 placeholder-gray-400 focus:outline-none focus:ring dark:border-gray-700 dark:placeholder-gray-200"
                     type="number"
-                    value={seconds}
+                    value={props.durationSeconds}
                     onChange={handleSecondsChange}
                      />
             </label>
